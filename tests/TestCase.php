71e7ea629b8b8278307ed816fa94e2dfae128ebc
Joseph Szobody <joseph@stechstudio.com>
2019-12-19 15:57:29 -0600

    fix tests

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -21,7 +21,7 @@ class TestCase extends \Orchestra\Testbench\TestCase
     /**
      *
      */
-    protected function setUp()
+    protected function setUp(): void
     {
         parent::setUp();
 
